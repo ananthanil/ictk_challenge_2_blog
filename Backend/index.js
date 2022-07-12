181bf2a5e3a3f453cae37d3d59d4870d668dc948
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-07-12 23:17:46 +0530

    addded database connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const BloggerData = require('./src/Model/blogdb')
+const cors = require('cors');
+var bodyparser = require('body-parser');
 var app = new express();
 var Port = 3002;
+app.use(cors());
+app.use(bodyparser.json())
+app.get('/Blogs',function(req,res){
+
+    BloggerData.find()
+               .then(function(blogger){
+                    res.send(blogger);
+               });
+});
 app.listen(Port,function(){
     console.log(`listening to port ${Port}`);
 })
--- Backend/src/Model/blogdb.js
+++ Backend/src/Model/blogdb.js
@@ -5,9 +5,13 @@ const Schema = mongoose.Schema;
 var NewBloggerSchema = new Schema({
     bloggerId : Number,
     bloggerName : String,
-    imageUrl : String
-});
+    imageUrl : String,
+    blogTitle : String,
+    blogDate : String,
+    blogContent : String
+})
 
-var Blogger = mongoose.model('Blogger', NewBloggerSchema); //UserData is the model and NewBookData is the schema
+var Blogger = mongoose.model('Blogger',NewBloggerSchema); //UserData is the model and NewBookData is the schema
 
 module.exports = Blogger;
+

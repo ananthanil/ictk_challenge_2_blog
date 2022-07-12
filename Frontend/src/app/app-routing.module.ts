71c599587af73640d13a33969a198df3e7ae6572
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-07-12 19:59:18 +0530

    added footer html code

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogviewComponent } from './blogview/blogview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:BlogviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

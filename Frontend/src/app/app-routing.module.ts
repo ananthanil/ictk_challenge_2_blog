9573bcdbdb2a8f5ca54ef71708ec93c8e81da3af
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-07-12 20:18:48 +0530

    added signin html

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogviewComponent } from './blogview/blogview.component';
+import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
-  {path:'',component:BlogviewComponent}
+  {path:'',component:BlogviewComponent},
+  {path:'signin',component:SigninComponent}
 ];
 
 @NgModule({
--- Frontend/src/app/signin/signin.component.ts
+++ Frontend/src/app/signin/signin.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-
+  title:String = "Sign In";
   constructor() { }
 
   ngOnInit(): void {
